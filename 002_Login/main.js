7c3fecd3a57f204c7414599470e50d1641ec96ab
kaiwei <freeave@163.com>
2026-06-04 18:46:54 +0800

    登录界面：AI可根据此实例生成游戏代码，包含大部分函数用法

--- 002_Login/main.js
+++ 002_Login/main.js
@@ -4,31 +4,35 @@
 // AI游戏生成：以实例为基础自动生成AI游戏代码，例如贪吃蛇，飞机大战等小游戏
 // AI提示文本："下面是开维游戏引擎的代码演示，根据这个代码，写一个sinx的函数演示代码。 代码如下：(拷贝以下代码)"
  
-// 初始化游戏引擎，根据平台设置屏幕分辨率
 // ----------------------------------------------------------------------------------------------
-var system = game.getSystemName(); // 获取系统名称
-var w, h; // 屏幕宽高
-var window;
+// 初始化引擎与环境
+// ----------------------------------------------------------------------------------------------
+var system = game.getSystemName();         // 获取系统名称：WINDOWS、WEB、WEIXIN
+var webOS = game.getOS();                  // web页面所在的操作系统：Windows、macOS、Linux、Android、iOS、Unknown OS
+var webDeviceType = game.getDeviceType();  // web页面所在的设备类型：PC、Phone、Pad
+var webDpr = game.getDpr();                // web页面dpr值
+var w, h;       // 屏幕宽高
+var window;     // 窗口变量
 var screenType; // 横屏还是竖屏
- 
-if (system =="WINDOWS")
-{
-    game.init() // windows默认窗口大小为800*600;web网页默认全屏
-    window = game.getWindow(); // 获取资源对象
-    w = window.getWidth();  // 屏幕宽带
-    h = window.getHeight(); // 屏幕高度
-}
-else if(system =="WEIXIN" || system =="WEB")
-{
-    game.initSize(canvas.width,canvas.height); // 微信窗口
-    window = game.getWindow(); // 获取资源对象
-    w = canvas.width; // 微信窗口宽度
-    h = canvas.height;// 微信窗口高度
+  
+// 根据平台不同，设置屏幕分辨率
+if (system == "WINDOWS")                       // windows平台
+    game.init();                               // windows默认窗口大小为800*600，自适应
+else if (system == "WEIXIN")                   // 微信小游戏
+    game.initSize(canvas.width,canvas.height); // 微信窗口大小，默认横屏844*390，竖屏390*844
+else if (system == "WEB") {                    // web平台，导出网页或安卓
+    if (webDeviceType == "PC")                 // PC机上的浏览器
+        game.init();                           // windows下浏览器默认窗口大小为800*600，自适应
+    else if (webDeviceType == "Phone")         // 手机上的浏览器
+        game.initSize(canvas.width/webDpr,canvas.height/webDpr); // 安卓导出时的逻辑分辨率
 }
  
-// 判断横屏还是竖屏
+// 获取屏幕宽度和高度
+window = game.getWindow(); // 获取资源对象
+w = window.getWidth();     // 屏幕宽带
+h = window.getHeight();    // 屏幕高度
 screenType = (w>h)?"Landscape":"Portrait"; // 横屏Landscape 竖屏Portrait
-game.setFPS(60); // 设置帧率
+game.setFPS(60);           // 设置帧率
  
 // 游戏场景设置
 var scene = new Scene(); // 新建场景1，登录界面
