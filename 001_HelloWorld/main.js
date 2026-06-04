1c338fa100b3ff00d5f63550b6e8e2c673b00e41
kaiwei <freeave@163.com>
2026-06-04 18:45:46 +0800

    Hello world 以下实例均可导出：exe、html、微信小游戏、安卓apk

--- 001_HelloWorld/main.js
+++ 001_HelloWorld/main.js
@@ -1,14 +1,34 @@
 // Hello world 程序实例
   
-// 初始化游戏引擎， 根据平台设置屏幕分辨率
-var system = game.getSystemName(); // 获取系统名称
-if (system =="WINDOWS")
-    game.init() // 默认窗口大小为800*600
-else if (system =="WEB" || system =="WEIXIN")
-    game.initSize(canvas.width,canvas.height); // 微信窗口。微信因为有大小限制，字体库需用工具精简大小
+// ----------------------------------------------------------------------------------------------
+// 初始化引擎与环境
+// ----------------------------------------------------------------------------------------------
+var system = game.getSystemName();         // 获取系统名称：WINDOWS、WEB、WEIXIN
+var webOS = game.getOS();                  // web页面所在的操作系统：Windows、macOS、Linux、Android、iOS、Unknown OS
+var webDeviceType = game.getDeviceType();  // web页面所在的设备类型：PC、Phone、Pad
+var webDpr = game.getDpr();                // web页面dpr值
+var w, h;       // 屏幕宽高
+var window;     // 窗口变量
+var screenType; // 横屏还是竖屏
+  
+// 根据平台不同，设置屏幕分辨率
+if (system == "WINDOWS")                       // windows平台
+    game.init();                               // windows默认窗口大小为800*600，自适应
+else if (system == "WEIXIN")                   // 微信小游戏
+    game.initSize(canvas.width,canvas.height); // 微信窗口大小，默认横屏844*390，竖屏390*844
+else if (system == "WEB") {                    // web平台，导出网页或安卓
+    if (webDeviceType == "PC")                 // PC机上的浏览器
+        game.init();                           // windows下浏览器默认窗口大小为800*600，自适应
+    else if (webDeviceType == "Phone")         // 手机上的浏览器
+        game.initSize(canvas.width/webDpr,canvas.height/webDpr); // 安卓导出时的逻辑分辨率
+}
  
-// 设置帧率
-game.setFPS(10); 
+// 获取屏幕宽度和高度
+window = game.getWindow(); // 获取资源对象
+w = window.getWidth();     // 屏幕宽带
+h = window.getHeight();    // 屏幕高度
+screenType = (w>h)?"Landscape":"Portrait"; // 横屏Landscape 竖屏Portrait
+game.setFPS(60);           // 设置帧率
  
 // 获取资源
 var res = game.getResource(); // 获取资源对象
